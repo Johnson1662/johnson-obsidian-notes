cf726626c9d082b9aa6217286fac659329e4d978
Johnson1662 <3261396640@qq.com>
2026-05-04 21:17:35 +0800

    fix(config): rename page title from Johnson1662 to Johnson

--- quartz.config.ts
+++ quartz.config.ts
@@ -8,7 +8,7 @@ import * as Plugin from "./quartz/plugins"
  */
 const config: QuartzConfig = {
   configuration: {
-    pageTitle: "Johnson1662",
+    pageTitle: "Johnson",
     pageTitleSuffix: "",
     enableSPA: true,
     enablePopovers: true,
